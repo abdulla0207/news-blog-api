c2fecea4529389dc1b02f83cf437ab73f3ea5333
abdulla0207 <abdulla.ermatov0407@gmail.com>
2023-03-26 15:53:50 +0500

    created profile entity and configured database connection

---
+++ src/main/java/com/company/entity/ProfileEntity.java
@@ -0,0 +1,37 @@
+package com.company.entity;
+
+import com.company.enums.ProfileRoleEnum;
+import com.company.enums.ProfileStatusEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "profile")
+@Getter
+@Setter
+public class ProfileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private String email;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column
+    private String password;
+    @Column
+    private ProfileStatusEnum status;
+    @Column
+    private ProfileRoleEnum role;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
