335e21994af397f207b02628a6a5d12fd5abd50a
abdulla0207 <abdulla.ermatov0407@gmail.com>
2024-01-30 14:06:42 +0500

    updating the endpoint func of getting top 8 articles by limit

---
+++ src/main/java/com/company/mapper/ArticleShortViewInfo.java
@@ -0,0 +1,42 @@
+package com.company.mapper;
+
+import java.time.LocalDateTime;
+
+public class ArticleShortViewInfo implements IArticleShortViewInfo{
+    private String uuid;
+    private String title;
+    private String description;
+    private LocalDateTime publishedDate;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDateTime publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+}
